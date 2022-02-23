5af19969ce48f0d7a06c9ac307a11413b95d3773
Jairinho <t4ngz3r0@gmail.com>
2022-02-23 19:39:14 -0300

    playing with libretro

--- Makefile
+++ Makefile
@@ -1,16 +1,17 @@
 TARGET=chip8_libretro
 LIBRETRO_CORE=$(TARGET).so
 LIBRETRO_HEADER=$(TARGET).h
+BUILD_VERSION=$(shell git rev-parse --short HEAD)
 
 # run: $(LIBRETRO_CORE)
 # 	retroarch -v -L $(LIBRETRO_CORE) roms/test_opcode.ch8
 
-$(LIBRETRO_CORE):
-	go build -buildmode=c-shared -o $@ ./libretro
-
-test:
-	go test -v -race ./chip8
+$(LIBRETRO_CORE): clean
+	go build -buildmode=c-shared -ldflags "-X 'main.BuildVersion=$(BUILD_VERSION)'" -o $@ ./libretro
 
 clean: 
 	rm -f $(LIBRETRO_CORE) $(LIBRETRO_HEADER)
+
+test:
+	go test -v -race ./chip8
 
--- libretro/libretro.go
+++ libretro/libretro.go
@@ -9,24 +9,40 @@ typedef struct retro_system_av_info retro_system_av_info;
 */
 import "C"
 import (
-	"fmt"
+	"github.com/tangzero/chip8-emulator/chip8"
 )
 
-const RetroAPIVersion = 1
+var BuildVersion string
+var emulator *chip8.Emulator
 
-//export retro_init
-func retro_init() {
-	fmt.Println("loading chip8 core...")
+//export Initialize
+func Initialize() {
+	emulator = chip8.NewEmulator()
 }
 
-//export retro_deinit
-func retro_deinit() {
-	fmt.Println("unloading chip8 core...")
+//export Deinitialize
+func Deinitialize() {
+	emulator = nil
 }
 
-//export retro_api_version
-func retro_api_version() uint32 {
-	return RetroAPIVersion
+//export GetEmulatorInfo
+func GetEmulatorInfo(info *C.retro_system_info) {
+	info.library_name = C.CString("CHIP-8 Emulator by TangZero")
+	info.library_version = C.CString(BuildVersion)
+	info.valid_extensions = C.CString("ch8")
+	info.need_fullpath = false
+	info.block_extract = false
+}
+
+//export GetEmulatorAVInfo
+func GetEmulatorAVInfo(info *C.retro_system_av_info) {
+	info.geometry.base_width = 64
+	info.geometry.base_height = 32
+	info.geometry.max_width = 64
+	info.geometry.max_height = 32
+	info.geometry.aspect_ratio = 0.0
+	info.timing.fps = 60
+	info.timing.sample_rate = 44100
 }
 
 func main() {}
