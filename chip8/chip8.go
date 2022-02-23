249486ee2493556da0311ce34b1c8616c3eafb4b
Jairinho <t4ngz3r0@gmail.com>
2022-02-23 13:50:03 -0300

    very basic emulator structure

--- chip8/chip8.go
+++ chip8/chip8.go
@@ -1,1 +1,35 @@
 package chip8
+
+type Emulator struct {
+	V      [16]uint8   // general registers
+	I      uint16      // address register
+	SP     uint16      // stack pointer
+	PC     uint16      // program counter
+	Memory [4096]uint8 // 4KB of system RAM
+	Stack  []uint8     // 32 bytes of stack. starting at 0x0EA0
+	Screen []uint8     // 256 bytes of display buffer. starting at 0x0F00
+	Timer  struct {
+		Delay uint8 // delay timer
+		Sound uint8 // sound timer
+	}
+	ROM []uint8
+}
+
+func NewEmulator() *Emulator {
+	emulator := new(Emulator)
+	emulator.Reset()
+	return emulator
+}
+
+func (emulator *Emulator) Reset() {
+	emulator.V = [16]uint8{}
+	emulator.I = 0x00
+	emulator.SP = 0x00
+	emulator.PC = 0x0200
+	emulator.Memory = [4096]uint8{}
+	emulator.Stack = emulator.Memory[0x0EA0:]
+	emulator.Screen = emulator.Memory[0x0F00:]
+	emulator.Timer.Delay = 0
+	emulator.Timer.Sound = 0
+	copy(emulator.Memory[0x0200:], emulator.ROM)
+}
--- chip8/chip8_test.go
+++ chip8/chip8_test.go
@@ -1,1 +1,19 @@
 package chip8_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tangzero/chip8-emulator/chip8"
+)
+
+func TestEmulator_Reset(t *testing.T) {
+	emulator := chip8.NewEmulator()
+	emulator.V[0x03] = 0xFF
+	emulator.V[0x0F] = 0xBB
+
+	emulator.Reset()
+
+	assert.Equal(t, uint8(0x00), emulator.V[0x03])
+	assert.Equal(t, uint8(0x00), emulator.V[0x0F])
+}
